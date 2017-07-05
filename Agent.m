695e4f696be303b2f54592b545d894f99e8f5194
Victor <victor@MacBook-Pro.local>
2017-07-05 14:09:08 +0800

    update

--- Agent.m
+++ Agent.m
@@ -7,34 +7,47 @@ classdef Agent
       mass
       pos
       vel
+      vel_init
+      %parameters defined here
+      %interaction strength
+      A = 2000;
+      %repulsive interaction
+      B = 0.08;
+      %elasticity constant
+      k = 120000;
+      %relative tangential velocity
+      kappa = 240000;
+      %relaxation time
+      tau = 0.5;
+      %
+      
    end
    methods
      %constructor of agent. in this function, the agent variables are
      %initially defined
-     function obj = Agent(rad, mass, pos)
-        %definition of default values. rad = 1, mass = 1, pos at origin, vel = 0
-        %velocity always zero at initialization
+     function obj = Agent(pos)
+        %definition if no parameters were inputted
         if nargin == 0
-            rad = 1;
+            %if position not defined, set agent at origin
             pos = [0,0];
-            vel = 0;
-            mass = 1;
-        end
-        %radius and mass should be positive
-        if (rad>0) && (mass>0)
-            obj.rad = rad;
-            obj.mass = mass;
-            obj.pos = pos;  
-            obj.vel = 0;
         else
-            error('Radius and Mass must be positive')
+            obj.pos = pos;
         end
+        %definition of default values
+        %radius between .25m and .35m
+        obj.rad = .25 + (.35-.25).*rand(1,1);
+        %velocity is 0
+        obj.vel = 0;
+        %mass set to 80kg
+        obj.mass = 80;
+        %initial velocity from 1m/s to 1.5m/s
+        obj.vel_init = 1 + (1.5-1).*rand(1,1);
      end
      %compute dv/dt with the social force model 
      function accelerate(allAgents)
          %sum of socForce
          for i = 1:length(allAgents)
-             
+         
          end
          %add components
 
